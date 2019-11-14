de9ba21c90edccb0a0cd25e4d06198979f39232d
Gheorghita Mutu <gheorghitamutu@gmail.com>
2019-11-14 16:10:37 +0200

    added Viridian project as API for Hyper-V VM actions and ViridiandTester in order to create the required test cases

---
+++ Viridian/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Viridian
+{
+    public class Class1
+    {
+    }
+}
---
+++ ViridianTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ViridianTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ViridianTester")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83fcb382-934c-4f25-92e6-b36b3d9238bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
