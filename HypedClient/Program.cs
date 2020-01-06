9ce2f6c3dced912a2536ff2d69a909d1ee90ff49
Gheorghita Mutu <gheorghitamutu@gmail.com>
2020-01-06 19:43:30 +0200

    added Serilog to client

--- HypedClient/Program.cs
+++ HypedClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Serilog;
+using System;
+using Serilog.Core;
 
 namespace HypedClient
 {
@@ -6,7 +9,25 @@ namespace HypedClient
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var levelSwitch = new LoggingLevelSwitch();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(levelSwitch)
+                .Enrich.WithProperty("InstanceId", Guid.NewGuid().ToString("n"))
+                // .WriteTo.BrowserHttp() // -> https://nblumhardt.com/2019/11/serilog-blazor | https://docs.microsoft.com/en-us/aspnet/core/blazor/debug?view=aspnetcore-3.1
+                .WriteTo.BrowserConsole(levelSwitch: levelSwitch)
+                .CreateLogger();
+
+            Log.Information("Hello, browser!");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An exception occurred while creating the WASM host");
+                throw;
+            }
         }
 
         public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
